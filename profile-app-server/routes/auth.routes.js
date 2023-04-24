8b85b49386f583e191da60907d627603ae20e858
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-24 19:14:59 +0200

    Set up routes, create signup route

--- profile-app-server/routes/auth.routes.js
+++ profile-app-server/routes/auth.routes.js
@@ -1,5 +1,78 @@
 const User = require('../models/User.model');
 const mongoose = require('mongoose');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+router.post('/signup', async (req, res, next) => {
+  const { username, password, campus, course } = req.body;
+
+  try {
+    if (password === '' || username === '' || campus === '' || course === '') {
+      res
+        .status(400)
+        .json({ message: 'Provide password, username, campus and course' });
+      return;
+    }
+
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message:
+          'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.',
+      });
+    }
+
+    const foundUser = await User.findOne({ username });
+
+    if (foundUser) {
+      res.status(400).json({ message: 'User already exists.' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const createdUser = await User.create({
+      username,
+      password: hashedPassword,
+      campus,
+      course,
+    });
+
+    const {
+      username: createdUsername,
+      password: createdPassword,
+      campus: createdCampus,
+      course: createdCourse,
+      _id: createdId,
+    } = createdUser;
+
+    //leave out password
+
+    const user = {
+      createdUsername,
+      createdCampus,
+      createdCourse,
+      createdId,
+    };
+
+    res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
+
+router.get('/verify', async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
 
 module.exports = router;
--- profile-app-server/routes/user.routes.js
+++ profile-app-server/routes/user.routes.js
@@ -2,4 +2,19 @@ const User = require('../models/User.model');
 const mongoose = require('mongoose');
 const router = require('express').Router();
 
+router.get('/users', async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
+
+router.put('/users', async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
+
+router.post('/upload', async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
+
 module.exports = router;
