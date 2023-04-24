b9b89b37c5d8a96213bcdde786cb433becccfbd3
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-24 19:00:30 +0200

    Create User Model

--- profile-app-server/models/User.model.js
+++ profile-app-server/models/User.model.js
@@ -1,26 +1,46 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
+    username: {
       type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
+      required: [true, 'Username is required'],
     },
+
     password: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, 'Password is required.'],
+    },
+
+    campus: {
+      type: String,
+      enum: [
+        'Madrid',
+        'Barcelona',
+        'Miami',
+        'Paris',
+        'Berlin',
+        'Amsterdam',
+        'México',
+        'Sao Paolo',
+        'Lisbon',
+        'Remote',
+      ],
+    },
+    course: {
+      type: String,
+      enum: ['Web Dev', 'UX/UI', 'Data Analytics', 'Cyber Security'],
+    },
+    image: {
+      type: String,
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
