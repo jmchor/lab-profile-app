9fb31261a004fdbd2afc6eb4a23639e0f9d0628f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-26 07:13:29 +0200

    Solve iteration 3, signup/login pages

--- profile-app-client/src/pages/LoginPage.jsx
+++ profile-app-client/src/pages/LoginPage.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5005';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const loginData = { username, password };
+
+    axios
+      .post(`${API_URL}/auth/login`, loginData)
+      .then((res) => {
+        console.log(res);
+        navigate('/');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLoginSubmit}>
+        <div className="form-group p-2 ">
+          <label htmlFor="username">Username</label>
+          <input
+            className="form-control"
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="btn btn-primary">Login</button>
+
+          <p>
+            Don't have an account yet?
+            <Link to="/signup">Signup</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
